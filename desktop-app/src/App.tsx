e4b2199ac753d4b224c7d3ffd01753cc47b79175
aquamaryne <67910638+aquamaryne@users.noreply.github.com>
2024-11-14 13:19:32 +0200

    updates

--- desktop-app/src/App.tsx
+++ desktop-app/src/App.tsx
@@ -1,11 +1,13 @@
 import Navbar from './components/navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import PageThree from './pages/page_three';
+import Instructions from './pages/instructions';
 function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
+        <Route path='/' element={<Instructions />} />
         <Route path='/pages/page_three' element={<PageThree />} />
       </Routes>
     </Router>
--- desktop-app/src/components/navbar.tsx
+++ desktop-app/src/components/navbar.tsx
@@ -5,16 +5,19 @@ import { Link } from 'react-router-dom';
 const Navbar: React.FC = () => {
     return(
         <Grid2>
+            <Link to='/'>
+                <Button>Instructions</Button>
+            </Link>
             <Link to='/pages/page_three'>
                 <Button>page 3</Button>
             </Link>
-            <Link to='/pages/'>
+            <Link to='/pages/page_four'>
                 <Button>page 4</Button>
             </Link>
-            <Link to='/pages/'>
+            <Link to='/pages/page_five'>
                 <Button>page 5</Button>
             </Link>
-            <Link to='/pages/'>
+            <Link to='/pages/page_six'>
                 <Button>page 6</Button>
             </Link>
         </Grid2>
